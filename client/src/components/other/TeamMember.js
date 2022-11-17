9c39c557965652a7116e1214ce9cb47421b52704
Jerome <derelia2004@gmail.com>
2022-11-17 18:43:15 +0100

    username system rework

--- client/src/components/other/TeamMember.js
+++ client/src/components/other/TeamMember.js
@@ -1,10 +1,21 @@
-import { deleteDoc, doc, updateDoc } from "firebase/firestore";
-import React from "react";
+import { async } from "@firebase/util";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  onSnapshot,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { firestore } from "../../config/firebase";
 import { useAuth } from "../../context/AuthContext";
 
 export const TeamMember = ({ email, document }) => {
   const { user } = useAuth();
+  const [contributer, setContributer] = useState("");
 
   const deleteMember = async () => {
     var oldContributor = [];
@@ -24,10 +35,25 @@ export const TeamMember = ({ email, document }) => {
     });
   };
 
+  //get contributer name
+  useEffect(() => {
+    const publicUsersRef = query(
+      collection(firestore, "publicUsers"),
+      where("email", "==", email)
+    );
+    onSnapshot(publicUsersRef, (snapshot) => {
+      setContributer();
+      setContributer(
+        snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))[0]
+      );
+      console.log("contributers read");
+    });
+  }, [email]);
+
   return (
     <div className="alert shadow-lg flex-row">
       <div className="w-2/3">
-        <svg
+        {contributer ? <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -40,8 +66,26 @@ export const TeamMember = ({ email, document }) => {
             strokeLinejoin="round"
             d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
           />
-        </svg>
-        <span className="truncate md:w-full">{email}</span>
+        </svg> : <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="red"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>}
+
+        {contributer ? (
+          <span className="truncate md:w-full">{contributer.displayName}</span>
+        ) : (
+          <span className="truncate md:w-full">{email}</span>
+        )}
       </div>
       <div className="flex-none md:hidden">
         {user.uid === document.userId && (
